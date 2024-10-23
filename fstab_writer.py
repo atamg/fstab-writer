d0a94f83172213d0905ecbb934260d578b32d2a8
Ata Mahmoudi <ata.mahmoudi@gmail.com>
2024-10-23 23:48:46 +0200

    Add argument parser

--- fstab_writer.py
+++ fstab_writer.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
-def yaml_to_fstab():
-    ...
+def yaml_to_fstab(yaml_file, fstab_file):
+    print(f"YAML file is: {yaml_file}")
+    print(f"fstab file is: {fstab_file}")
 
 
 def main():
+    # Import argparse module to read command-line arguments
+    import argparse
 
-    #Pars command-line arguments and call yaml_to_fstab() method
-    ...
+    # Initialize parser
+    parser = argparse.ArgumentParser(description="Process YAML to generate /etc/fstab file")
+
+    # Add arguments (first argument, yaml_file, is mandatory)
+    parser.add_argument('yaml_file', type=str, help='Path to YAML file')
+    parser.add_argument('--fstab_file', type=str, default='/etc/fstab', help='Path to fstab file')
+
+    # Read command-line arguments and return args object
+    args = parser.parse_args()
+
+    # Call yaml_to_fstab function
+    yaml_to_fstab(args.yaml_file, args.fstab_file)
+
 
 if __name__ == "__main__":
     main()
