2ab7608960fe2af37085678e726302cf00d2878a
Ata Mahmoudi <ata.mahmoudi@gmail.com>
2024-10-23 22:58:59 +0200

    Add fstab_writer.py

---
+++ fstab_writer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def yaml_to_fstab():
+    ...
+
+
+def main():
+
+    #Pars command-line arguments and call yaml_to_fstab() method
+    ...
+
+if __name__ == "__main__":
+    main()
+
