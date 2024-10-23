b58c6de78ba9f383dee48e6583af3e5d435bee5d
Ata Mahmoudi <ata.mahmoudi@gmail.com>
2024-10-24 01:50:33 +0200

    Add parse_yaml_file function

--- fstab_writer.py
+++ fstab_writer.py
@@ -1,8 +1,55 @@
 #!/usr/bin/env python3
 
+import re
+import sys
+import pprint
+
+CONFIG = {
+    "device_pattern": r'^(/[^:]+|((25[0-5]|2[0-5][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])):$',  # Regex pattern for block devices, IPs
+    "uuid_pattern": r'^(LABEL|UUID|PARTUUID|PARTLABEL)=[\w-]+$',  # Regex pattern for UUIDs, LABELs, PARTUUIDs, PARTLABELs
+    "key_value_patten": r'^([\w-]+):\s*(.*)$' # Regex pattern for key-value for each device
+}
+
+def parse_yaml_file(yaml_file):
+    fstab_dict = {}
+    current_device = None
+    
+    try:
+        # Read YAML file
+        with open(yaml_file, 'r') as file:
+
+            for line in file:
+                # Remove whitespaces
+                line = line.strip()
+
+                if re.match(CONFIG['device_pattern'], line) or re.match(CONFIG['uuid_pattern'], line):
+                    current_device = line[:-1].strip() if line.endswith(':') else line.strip()
+                    fstab_dict[current_device] = {} # Initialize dict for device
+                elif current_device:
+                    key_value_match = re.match(CONFIG['key_value_patten'], line)
+                    if key_value_match:
+                        key, value = key_value_match.groups() # Assign matched key and value of current line
+
+                        if key == 'options':
+                            fstab_dict[current_device][key] = [] # Initialize options key
+                        else:
+                            fstab_dict[current_device][key] = value.strip()
+                    elif line.startswith('-'):
+                        option = line[1:].strip()
+                        fstab_dict[current_device]['options'].append(option)
+
+        return fstab_dict
+
+    
+    except Exception as e:
+        print(f"Error while parsing YAML file: {str(e)}")
+        sys.exit(1)
+
+
 def yaml_to_fstab(yaml_file, fstab_file):
-    print(f"YAML file is: {yaml_file}")
-    print(f"fstab file is: {fstab_file}")
+    
+    parsed_fstab = parse_yaml_file(yaml_file)
+    pprint.pprint(parsed_fstab)
 
 
 def main():
